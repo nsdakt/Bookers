7277e2b365238b577878842fd80fcafa4a385226
nsdakt <dnj.0077@gmail.com>
2021-09-06 14:56:28 +0000

    [update]top

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,10 @@
 class HomesController < ApplicationController
   def top
+    @homes = Home.all
+  end
+
+  private
+  def book_params
+   params.require(:home).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_103807) do
+ActiveRecord::Schema.define(version: 2021_09_06_074914) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
