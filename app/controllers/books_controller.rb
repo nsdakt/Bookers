7e75bcc70ac5f88171c426ce4ceda1908701b845
nsdakt <dnj.0077@gmail.com>
2021-09-06 17:42:08 +0000

    [update]show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -10,14 +13,36 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
-  private
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy!
+    redirect_to books_path
+  end
+
+private
   def book_params
-   params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
-
 end
