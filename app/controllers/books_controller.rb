e2c28ffe7901c8401be15faafecf56817f26fa20
nsdakt <dnj.0077@gmail.com>
2021-09-12 18:43:02 +0000

    [update]errorclear

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,13 +15,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(book.id)
+      redirect_to @book
     else
       flash.now[:alert] = "Title can't be blank"
-      render :new
+      render :index
     end
   end
 
@@ -30,10 +31,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = 'Book was successfully updated.'
-      redirect_to book_path(book.id)
+      redirect_to @book
     else
       render :edit
     end
